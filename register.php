914f994329dfdbd5cd7949f866f838efa7788599
vinoth-rlingam <vinoth.rlingam@gmail.com>
2017-02-28 19:07:02 +0530

    Swiftmailer

--- register.php
+++ register.php
@@ -1,9 +1,93 @@
 <?PHP
-$hi="hi";
-echo $hi;
+require_once("include/class.phpmailer.php");
 if(isset($_POST['Submit'])){
 	
-	function Sanitize($str,$remove_nl=true)
+	RegisterUser();
+}
+
+function GetFromAddress(){
+
+        $host = 'vrcloudtech.net';
+
+        $from ="donotreply@$host";
+		echo $from;
+        return $from;
+    } 
+
+
+function SendUserConfirmationEmail(&$formvars)
+
+    {
+		echo"Inside send mail";
+		
+        $mailer = new PHPMailer();
+		$mailer->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mailer->SMTPAuth = true; // authentication enabled
+$mailer->isSMTP();
+$mailer->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+$mailer->Host = "ssl://54.243.97.84";
+$mailer->Port = 25; // or 587
+$mailer->IsHTML(true);
+$mailer->Username = "AKIAJN3ANGOQT4W3NT3A";
+$mailer->Password = "Aj7qf62z65H90nU3nrBmVGsk0SxI3WksMzarJ8byg+Sd";
+
+
+
+
+       
+        $mailer->CharSet = 'utf-8';
+        
+        $mailer->AddAddress($formvars['email'],$formvars['name']);
+        
+        $mailer->Subject = "Your registration with ".'www.vrcloudtech.net';
+
+        $mailer->From = GetFromAddress();        
+        
+        $confirmcode = $formvars['confirmcode'];
+		
+        
+        $confirm_url = GetAbsoluteURLFolder().'/confirmreg.php?code='.$confirmcode;
+		echo $confirm_url;
+        
+        $mailer->Body ="Hello ".$formvars['name']."\r\n\r\n".
+        "Thanks for your registration with "."www.vrcloudtech.net"."\r\n".
+        "Please click the link below to confirm your registration.\r\n".
+        "$confirm_url\r\n".
+        "\r\n".
+        "Regards,\r\n".
+        "Webmaster\r\n".
+        "www.vrcloudtech.net";
+		if (!$mailer->Send()){
+			echo "mail failed";
+		}
+        return true;
+    }
+    function GetAbsoluteURLFolder()
+    {
+        $scriptFolder = (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on')) ? 'https://' : 'http://';
+        $scriptFolder .= $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']);
+		
+        return $scriptFolder;
+    }
+function RegisterUser(){
+$formvars = array();
+
+    {
+        $formvars['name'] = Sanitize($_POST['fullName']);
+		
+        $formvars['email'] = Sanitize($_POST['email']);
+        $formvars['username'] = Sanitize($_POST['userName']);
+        $formvars['password'] = Sanitize($_POST['password']);
+		
+		
+    }
+	InsertIntoDB($formvars);
+	SendUserConfirmationEmail($formvars);
+	return true;
+	
+}
+
+function Sanitize($str,$remove_nl=true)
     {
         $str = RemoveSlashes($str);
 
@@ -31,19 +115,13 @@ function RemoveSlashes($str)
         }
         return $str;
     }        
-$formvars = array();
-
+ function RedirectToURL($url)
     {
-        $formvars['name'] = Sanitize($_POST['fullName']);
-		echo $formvars['name'];
-        $formvars['email'] = Sanitize($_POST['email']);
-        $formvars['username'] = Sanitize($_POST['userName']);
-        $formvars['password'] = Sanitize($_POST['password']);
-		
+        header("Location: $url");
+        exit;
     }
-	InsertIntoDB($formvars);
-}
-
+    
+    
 function SanitizeForSQL($str)
     {
         if( function_exists( "mysql_real_escape_string" ) )
@@ -56,6 +134,15 @@ function SanitizeForSQL($str)
         }
         return $ret_str;
     }
+	
+	function MakeConfirmationMd5($email)
+    {   
+	
+	$rand_key = '0iQx5oBk66oVZep';
+        $randno1 = rand();
+        $randno2 = rand();
+        return md5($email.$rand_key.$randno1.''.$randno2);
+    }
 
 function InsertIntoDB(&$formvars)
     {
@@ -64,10 +151,13 @@ function InsertIntoDB(&$formvars)
 		$pwd="";
 		$database="";
 		
+		$confirmcode = MakeConfirmationMd5($formvars['email']);
 		
-        $db_host  = '127.0.0.1';
-        $username = 'root';
-        $pwd  = '';
+		$formvars['confirmcode'] = $confirmcode;
+		
+        $db_host  = 'vrcloudtech.cntcf3hj2a6p.ap-south-1.rds.amazonaws.com:3306';
+        $username = 'vrcloudtech';
+        $pwd  = 'vrcloudtech';
         $database  = 'vrcloudtech';
         $tablename = 'registeruser';
 		$connection = mysqli_connect($db_host,$username,$pwd,$database);
@@ -78,17 +168,22 @@ function InsertIntoDB(&$formvars)
                 name,
                 email,
                 username,
-                password
+                password,
+				confirmcode
                 )
                 values
                 (
                 "' . SanitizeForSQL($formvars['name']) . '",
                 "' . SanitizeForSQL($formvars['email']) . '",
                 "' . SanitizeForSQL($formvars['username']) . '",
-                "' . md5($formvars['password']) . '"
+                "' . md5($formvars['password']) . '",
+				"' . $formvars['confirmcode'] . '"
                 )';   
+			
 				$insert = $connection->query($insert_query);
-				echo $insert_query;
+				
+				
+				
 	}
 	
 
